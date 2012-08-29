6f443e47530a9c972be4658f7ddf5a25cd04550c
mrstealth <mrstealth@gmx.net>
2012-08-29 13:37:07 +0200

    show categories (WIP)

--- plugin.video.filin.tv/default.py
+++ plugin.video.filin.tv/default.py
@@ -19,11 +19,15 @@ __addon__    = xbmcaddon.Addon(id='plugin.video.filin.tv')
 
 URL         = 'http://www.filin.tv'
 
+# TODO: find a better way of html decoding
+#def format(text):
+#    return re.sub(r'^(&.*;)$', '', text)
+
 def unescape(entity, encoding):
   if encoding == 'utf-8':
     return HTMLParser.HTMLParser().unescape(entity).encode(encoding)
   elif encoding == 'cp1251':
-    return HTMLParser.HTMLParser().unescape(entity).decode(encoding).encode('utf-8')
+    return entity.decode(encoding).encode('utf-8')
 
 def get_url(string):
   return re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+.xml', string)[0]
@@ -35,7 +39,40 @@ def next(url):
     return URL+'/page/' + str(int(url[-1])+1)
 
 
-# (xbmc.getSkinDir() == "skin.quartz")    
+# (xbmc.getSkinDir() == "skin.quartz")
+
+def menu():
+    name="Categories"
+    item = xbmcgui.ListItem(name)
+    uri = sys.argv[0] + '?mode=GENRES'
+    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+
+    name='Latest income'
+    item = xbmcgui.ListItem(name)
+    uri = sys.argv[0] + '?mode=RECENT'
+    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+def categories(url):
+    page = ''
+    result = common.fetchPage({"link": url})
+
+    if result["status"] == 200:
+        content = common.parseDOM(result["content"], "div", attrs = { "class":"mcont" })
+        categories = common.parseDOM(content, "option", ret="value")
+        descriptions = common.parseDOM(content, "option")
+
+        if len(categories):
+            for i, categorie in enumerate(categories):
+                uri = sys.argv[0] + '?mode=RECENT&url=' + URL + '/x.php?onlyjanr=' + categorie
+                title = unescape(descriptions[i], 'cp1251')
+                print uri
+
+                item = xbmcgui.ListItem(title)
+                xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+
+    xbmcplugin.endOfDirectory(pluginhandle, True)
 
 def recent(url):
     page = ''
@@ -45,26 +82,23 @@ def recent(url):
         content = common.parseDOM(result["content"], "div", attrs = { "id":"dle-content" })
         mainf = common.parseDOM(content, "div", attrs = { "class":"mainf" })
         block = common.parseDOM(content, "div", attrs = { "class":"block_text" })
-        
-        info = common.parseDOM(result["content"], "div", attrs = { "style":"display:inline;" })
- 
-    
-        
-                        
+
+        descriptions = common.parseDOM(result["content"], "div", attrs = { "style":"display:inline;" })
+
         if len(mainf):
             for i, div in enumerate(mainf):
                 href = common.parseDOM(div, "a", ret="href")[0]
                 thumbnail = common.parseDOM(block[i], "img", ret = "src")[0]
                 if thumbnail[0] == '/': thumbnail = URL+thumbnail
-                 
+
                 # TODO: parse encoding from html meta tag
                 title = unescape(common.parseDOM(div, "a")[0], 'cp1251')
                 uri = sys.argv[0] + '?mode=SHOW&url=' + href + '&thumbnail=' + thumbnail
 
                 item = xbmcgui.ListItem(title, thumbnailImage=thumbnail)
                 item.setProperty( "Fanart_Image", thumbnail )
-                
-                item.setInfo( type='Video', infoLabels={'title': title, 'plot': info[i].decode('cp1251').encode('utf-8')})
+
+                item.setInfo( type='Video', infoLabels={'title': title, 'plot': unescape(descriptions[i], 'cp1251')})
                 xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
 
     uri = sys.argv[0] + '?mode=NEXT&url=' + next(url)
@@ -80,9 +114,22 @@ def show(url,thumbnail):
     #TODO: parse <creator> tag and create groups
     #http://filin.tv/208950482a70e02fad0e82e9a7db82c2/play/srochnoeuvedomlenie.xml
 
-    xml = common.fetchPage({"link": url})
-    locations = common.parseDOM(xml["content"], "location")
-    titles = common.parseDOM(xml["content"], "title")
+
+    xml = common.fetchPage({"link": url})["content"]
+    locations = common.parseDOM(xml, "location")
+    titles = common.parseDOM(xml, "title")
+
+    t = common.parseDOM(xml, "title")
+    creators = common.parseDOM(xml, "creator")
+
+    print type(creators)
+    for i in range(0, len(creators)):
+        print unescape(creators[i], 'utf-8')
+
+#        uri = sys.argv[0] + '?mode=PLAY&url=%s'%locations[i]
+#        item = xbmcgui.ListItem(unescape(titles[i], 'utf-8'), thumbnailImage=thumbnail)
+#        xbmcplugin.addDirectoryItem(pluginhandle, uri, item)
+#        item.setProperty('IsPlayable', 'true')
 
     for i in range(0, len(locations)):
         uri = sys.argv[0] + '?mode=PLAY&url=%s'%locations[i]
@@ -138,5 +185,9 @@ elif mode == 'SHOW':
     show(url,thumbnail)
 elif mode == 'PLAY':
     play(url)
-elif mode == None:
+elif mode == 'GENRES':
+    categories(URL)
+elif mode == 'RECENT':
     recent(URL)
+elif mode == None:
+    menu()
